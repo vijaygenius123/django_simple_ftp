95237d7cf57011d7d9f4a9503f24317d44a9211a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-06 19:23:34 +0530

    Error Handling On File Deletion

--- ftp/views.py
+++ ftp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.generics import DestroyAPIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
 from os import listdir, remove
+
 
 # Create your views here.
 
@@ -9,13 +11,17 @@ def home(request):
     files = listdir('./files')
     return render(request, 'ftp/home.html', {'files': files})
 
+
 class DeleteFileView(DestroyAPIView):
     permission_classes = []
 
     def delete(self, request, *args, **kwargs):
         filename = request.data.get('filename')
         if filename:
-            remove('./files/' + filename)
-            return Response({'Msg': 'Success'})
+            try:
+                remove('./files/' + filename)
+                return Response({'Msg': 'Success'})
+            except OSError:
+                return Response({'Msg': 'Error'}, HTTP_404_NOT_FOUND)
         else:
-            return Response({'Msg': 'Error'})
+            return Response({'Msg': 'Error'}, HTTP_404_NOT_FOUND)
